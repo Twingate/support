61868a957ceff074b53eb6097dcda3cb2dcc9efe
Aaron Griffin <griffinaaronj@gmail.com>
2022-03-10 16:43:36 -0700

    Update preferIPv4.ps1

--- Client/Windows/preferIPv4.ps1
+++ Client/Windows/preferIPv4.ps1
@@ -14,27 +14,27 @@ function Set-PreferIPv4 () {
       elseif (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters' | Select-Object -ExpandProperty 'DisabledComponents' -ErrorAction SilentlyContinue) {
         Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0x20 -ErrorAction Stop
         Write-Host "Setting IPv4 preference over IPv6."  -ForegroundColor Green
-        Write-Host "Reboot required for changes to take effect." -ForegroundColor Yellow
+        Write-Host "Reboot required for changes to take effect." -ForegroundColor Red
       }
       else {
         try {
           New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0x20 -PropertyType "DWord" -ErrorAction Stop
           Write-Host "Setting IPv4 preference over IPv6."  -ForegroundColor Green
-          Write-Host "Reboot required for changes to take effect." -ForegroundColor Yellow
+          Write-Host "Reboot required for changes to take effect." -ForegroundColor Red
         }
         catch {
             if ($($PSItem.ToString()) -eq "Requested registry access is not allowed.") {
-                Write-Output "Error: $($PSItem.ToString()) Powershell must be ran as administrator to run this script."   
+                Write-Output "Error: $($PSItem.ToString()) Powershell must be ran as administrator to run this script." -ForegroundColor Red
               } 
               else {
-                Write-Output "Error: $($PSItem.ToString())"    
+                Write-Output "Error: $($PSItem.ToString())" -ForegroundColor Red
               }
         }
       }
     }
     catch {
       if ($($PSItem.ToString()) -eq "Requested registry access is not allowed.") {
-        Write-Output "Error: $($PSItem.ToString()) Powershell must be ran as administrator to run this script."   
+        Write-Output "Error: $($PSItem.ToString()) Powershell must be ran as administrator to run this script." -ForegroundColor Red  
       } 
       else {
         Write-Output "Error: $($PSItem.ToString())"    
